4f218b3bf54bf5cb36c5e02f6e3800b28130b790
MariaG <MGrigorenko@bellintegrator.com>
2022-11-14 00:26:32 +0300

    edited TicketRepository.java & added NotFoundException.java

---
+++ src/main/java/ru/netology/repository/NotFoundException.java
@@ -0,0 +1,11 @@
+package ru.netology.repository;
+
+    public class NotFoundException extends RuntimeException {
+
+        public NotFoundException(String msg) {
+            super(msg);
+        }
+
+
+    }
+

6c2bd5a0d673b40ed3f06b0b872745aa3672512e
MariaG <MGrigorenko@bellintegrator.com>
2022-11-13 18:44:53 +0300

    added classes and edited Ticket.java

---
+++ src/main/java/ru/netology/domain/Ticket.java
@@ -0,0 +1,69 @@
+package ru.netology.domain;
+
+public class Ticket implements Comparable <Ticket>{
+    private int id;
+    private int price;
+    private String departureAirport;
+    private String arrivalAirport;
+    private int time;
+
+    public Ticket(int id, int price, String departureAirport, String arrivalAirport, int time) {
+        this.id = id;
+        this.price = price;
+        this.departureAirport= departureAirport;
+        this.arrivalAirport = arrivalAirport;
+        this.time = time;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getDepartureAirport() {
+        return departureAirport;
+    }
+
+    public void setDepartureAirport(String departureAirport) {
+        this.departureAirport = departureAirport;
+    }
+
+    public String getArrivalAirport() {
+        return arrivalAirport;
+    }
+
+    public void setArrivalAirport(String arrivalAirport) {
+        this.arrivalAirport = arrivalAirport;
+    }
+
+    public int getTime() {
+        return time;
+    }
+
+    public void setTime(int time) {
+        this.time = time;
+    }
+
+    @Override
+    public int compareTo(Ticket o) {
+        if (this.price < o.price) {
+            return -1;
+        } else if (this.price > o.price) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+}
